e5d803cf35c6dee539b796ea73e1b96a4dce118b
FranciG <beodo0@gmail.com>
2020-06-17 12:14:50 +0200

    Graph for price evolution

--- product_graph_test.php
+++ product_graph_test.php
@@ -1,5 +1,8 @@
 <?php
-   require_once("../includes/common.php");
+  
+
+  
+  require_once("../includes/common.php");
   $product_main['ean'] = '00000000271686';
 
 
@@ -11,7 +14,7 @@ die("Connection failed: " . $mysqli->error);
 }
 
 //Query
-$query = ("SELECT * FROM graph WHERE ean IN ('$product_main['ean']') group by DAY(created_at);");
+$query = ("SELECT * FROM graph WHERE ean IN ('$product_main[ean]') group by DAY(created_at);") or die(mysql_error());
 
 
 
@@ -20,13 +23,78 @@ $result = mysqli_query($mysqli, $query);
 
 
 
-//Only to display the results
+/* //Only to display the results
 while ($row=mysqli_fetch_assoc ($result)) {
-echo "{$row['ean']}<br>";
-}
+//echo "{$row['ean']}<br>";
+echo "['".$row["ean"]."', ".$row["created_at"].",".$row["avg_price"]."],<br>";
+} */
 
 ?>   
 
+<!DOCTYPE html>
+<html>
+<head> 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MySQL graphics</title>
+    <html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Fecha/hora', 'Precio'],
+          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["created_at"]."', ".$row["avg_price"]."],";  
+                          }  
+                          ?>  
+        ]);
+
+        var options = {
+          chart: {
+            title: 'El grafico representa la evolución de precios del producto en funcion del tiempo',
+            subtitle: 'Format: Day/Month/Year - Hour',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+
+
+
+
+
+
+
+     
+
+
+    </script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+
+
+<body>
+
+<div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+<br>
+<br>
+
+
+</body>
+</html>
 
    
